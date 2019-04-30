8e2dc82c34995128f29d7ed74c7f13dae1727edf
benouaer <tech@benouaer.com>
2019-04-30 19:17:39 +0100

    add start page

--- src/frontend/src/app/app.module.ts
+++ src/frontend/src/app/app.module.ts
@@ -1,18 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { DocumentUploadComponent } from './document-upload/document-upload.component';
+import { StartPageComponent } from './start-page/start-page.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    StartPageComponent,
+    DocumentUploadComponent
+  ],
   imports: [
     BrowserModule,
     FormsModule,
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: '', component: StartPageComponent },
+      { path: 'document-upload', component: DocumentUploadComponent }
+    ]),
   ],
   bootstrap: [AppComponent]
 })
--- src/frontend/src/app/start-page/start-page.component.ts
+++ src/frontend/src/app/start-page/start-page.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<router-outlet></router-outlet>'
+  templateUrl: './start-page.component.html'
 })
-export class AppComponent {
+export class StartPageComponent {
   constructor() {}
 }
